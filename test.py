0f4be4e3b1c471fd5bcccd19263a8aefd091df25
DodyMaulanaRamadhan <dodimaulanaramadhan10@gmail.com>
2024-07-25 16:22:50 +0700

    berubah

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-uasiubgduiasbudbuiad
-askjdbkajsbdkjasbdkajsd
-asjdkb aksjd akjsd a sorted
-kjs dkaskjdkajs GeneratorExit
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 9, 16, 25]
+
+plt.plot(x, y)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Grafik Sederhana')
+plt.show()
